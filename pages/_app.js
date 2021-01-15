235004ece3e98f87a5759f0402927bd58b8a177d
Kyungyoon Ha <gkb10a@gmail.com>
2021-01-15 09:33:02 +0900

    2021-01-15

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Head from "next/Head";
 import "antd/dist/antd.css";
 
+import wrapper from "../store/configureStore";
 const NodeBird = ({ Component }) => {
     return (
         <>
@@ -18,4 +19,4 @@ const NodeBird = ({ Component }) => {
 NodeBird.propTypes = {
     Component: PropTypes.elementType.isRequired,
 };
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
